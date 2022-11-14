06bfe21250b5e035aae3492e43f439429b048535
Davide Avenia <d.avenia.97@gmail.com>
2022-11-14 15:43:58 +0100

    Setup configuration

--- demo-bank-challenge/src/test/java/com/example/innotek/demobankchallenge/DemoBankChallengeApplicationTests.java
+++ demo-bank-challenge/src/test/java/com/example/innotek/demobankchallenge/DemoBankChallengeApplicationTests.java
@@ -1,13 +1,9 @@
-package com.example.innotek.demobankchallenge;
+package java.com.example.innotek.demobankchallenge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoBankChallengeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
