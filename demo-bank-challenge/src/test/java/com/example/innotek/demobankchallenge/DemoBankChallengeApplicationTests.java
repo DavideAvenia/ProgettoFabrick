421591f700192573ac09e0fb985b475305995227
Davide Avenia <d.avenia.97@gmail.com>
2022-11-14 16:20:44 +0100

    Setup completed

--- demo-bank-challenge/src/test/java/com/example/innotek/demobankchallenge/DemoBankChallengeApplicationTests.java
+++ demo-bank-challenge/src/test/java/com/example/innotek/demobankchallenge/DemoBankChallengeApplicationTests.java
@@ -1,6 +1,5 @@
-package java.com.example.innotek.demobankchallenge;
+package com.example.innotek.demobankchallenge;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
