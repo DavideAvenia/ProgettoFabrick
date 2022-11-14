9566c887ea5472801cec0dda8902691e13624404
Davide Avenia <d.avenia.97@gmail.com>
2022-11-14 14:57:17 +0100

    init project

---
+++ demo-bank-challenge/src/main/java/com/example/innotek/demobankchallenge/DemoBankChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.innotek.demobankchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBankChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBankChallengeApplication.class, args);
+	}
+
+}
---
+++ demo-bank-challenge/src/test/java/com/example/innotek/demobankchallenge/DemoBankChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.innotek.demobankchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
